9d0c2e6186c0706588ba9694192fa2baf0f753f7
Kushal Ghosh <kushalghosh075@gmail.com>
2024-06-08 22:42:54 +0530

    fix2

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,13 @@ const App = () => {
         Now I can render any React component on any DOM node I want using
         ReactDOM.render
       </p>
+      <form id="info-form">
+            <input type="text" id="full_name" placeholder='Full Name' />
+            <input type="email" id="email" placeholder='Email'/>
+            <input type="password" id="password" placeholder='Password' />
+            <input type="password" id="password_confirmation" placeholder='Password Confirmation' />
+            <button type='submit' onClick={()=>navigate('/form-ref')}>Submit</button>
+        </form>
       <BrowserRouter>
         <Routes>            
           <Route path="/" element={<Card />} />
