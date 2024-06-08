cc7a824eb4fab2d4d04438e7c8f80f43786323e6
Kushal Ghosh <kushalghosh075@gmail.com>
2024-06-08 22:06:29 +0530

    done

--- src/components/FormRef.js
+++ src/components/FormRef.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const FormRef = () => {
+    const navigate = useNavigate();
   return (
     <div >
         <Link id="form-link" to={'/form'}>form</Link>
       <Link id="form-ref-link" to={'/form-ref'}>formRef</Link>
       <Link id="form-state-link" to={'/form-state'}>formState</Link>
-        <form action="/formState" id="info-form">
+        <form id="info-form">
             <input type="text" id="full_name" placeholder='Full Name' />
             <input type="email" id="email" placeholder='Email'/>
             <input type="password" id="password" placeholder='Password' />
             <input type="password" id="password_confirmation" placeholder='Password Confirmation' />
-            <button type='submit'>Submit</button>
+            <button type='submit' onClick={()=>navigate('/form-state')}>Submit</button>
         </form>
     </div>
   )
--- src/components/FormState.js
+++ src/components/FormState.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const FormState = () => {
+    const navigate = useNavigate();
   return (
     <div >
         <Link id="form-link" to={'/form'}>form</Link>
       <Link id="form-ref-link" to={'/form-ref'}>formRef</Link>
       <Link id="form-state-link" to={'/form-state'}>formState</Link>
+      <form id="info-form">
+            <input type="text" id="full_name" placeholder='Full Name' />
+            <input type="email" id="email" placeholder='Email'/>
+            <input type="password" id="password" placeholder='Password' />
+            <input type="password" id="password_confirmation" placeholder='Password Confirmation' />
+            <button type='submit'>Submit</button>
+        </form>
     </div>
   )
 }
